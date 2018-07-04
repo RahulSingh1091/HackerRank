659e61d96ece7393514f5eefea0b49b838da578b
Rahul Singh <39204571+RahulSingh1091@users.noreply.github.com>
2018-07-04 20:50:27 +0530

    Create Day 1: Data Types.py

---
+++ Day 1: Data Types.py
@@ -0,0 +1,12 @@
+
+int_2 = input()
+double_2 = input()
+string_2 = raw_input()
+
+sum_int = i + int_2
+sum_double = d + double_2
+sum_string = s + string_2
+
+print sum_int
+print sum_double
+print sum_string
